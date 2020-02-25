5a3fe3182938599bd4937b403b4a6fab893d5c18
Aaron Keesing <agkphysics@gmail.com>
2020-02-26 01:31:44 +1300

    View results per dataset by recursively finding CSVs.

--- scripts/view_results.py
+++ scripts/view_results.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python3
 
 import argparse
-import os
-from math import sqrt, ceil
-from glob import glob
+from pathlib import Path
 
 import pandas as pd
 from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument('directory')
+parser.add_argument('directory', nargs='+')
 
 
 def main():
     args = parser.parse_args()
 
-    dirs = sorted([d for d in glob(os.path.join(args.directory, '*'))
-                   if os.path.isdir(d)])
-    n_rows = round(sqrt(len(dirs)))
-    n_cols = ceil(len(dirs) / n_rows)
-    fig: plt.Figure = plt.figure(figsize=(12, 6))
-    for idx, d in enumerate(dirs):
+    dirs = sorted([Path(d) for d in args.directory])
+    for d in dirs:
+        table = {}
         df_list = {}
-        for filename in sorted(glob(os.path.join(d, '*.csv'))):
-            name = os.path.basename(filename)
-            name = os.path.splitext(name)[0]
+        for filename in sorted(d.glob('**/*.csv')):
+            name = filename.relative_to(d).with_suffix('')
             df_list[name] = pd.read_csv(filename, header=[0, 1, 2, 3],
                                         index_col=0)
+        if not df_list:
+            continue
         uar = {c: df_list[c][('uar', 'all')].mean(1) for c in df_list}
 
-        ax: plt.Axes = fig.add_subplot(n_rows, n_cols, idx + 1)
-        ax.set_title(os.path.basename(d))
+        for name in df_list:
+            config = Path(name).name
+            clf = str(Path(name).parent)
+            table[(clf, config)] = uar[name].mean()
+
+        fig: plt.Figure = plt.figure()
+        ax: plt.Axes = fig.add_subplot(1, 1, 1)
+        ax.set_title(d.name)
         ax.boxplot(uar.values(), labels=uar.keys(), notch=True, bootstrap=1000)
-        ax.set_xticklabels(uar.keys(), rotation=45)
+        ax.set_xticklabels(uar.keys(), rotation=90)
         ax.set_xlabel('Config')
         ax.set_ylabel('UAR')
         fig.tight_layout()
+
+        df = pd.DataFrame(table.values(), index=table.keys()).unstack()
+        print(df)
     plt.show()
 
 
