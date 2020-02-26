b550bef116814717bd08faec40f02e25f5a8eb47
Aaron Keesing <agkphysics@gmail.com>
2020-02-26 16:35:03 +1300

    Add latex table output functionality.

--- scripts/view_results.py
+++ scripts/view_results.py
@@ -8,12 +8,17 @@ from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', nargs='+')
+parser.add_argument('--show', help="Show matplotlib figures",
+                    action='store_true')
+parser.add_argument('-o', '--output', type=str,
+                    help="Directory to write output summary tables.")
 
 
 def main():
     args = parser.parse_args()
 
     dirs = sorted([Path(d) for d in args.directory])
+    uar_list = []
     for d in dirs:
         table = {}
         df_list = {}
@@ -39,9 +44,33 @@ def main():
         ax.set_ylabel('UAR')
         fig.tight_layout()
 
-        df = pd.DataFrame(table.values(), index=table.keys()).unstack()
-        print(df)
-    plt.show()
+        df_uar = pd.Series(list(table.values()), index=table.keys())
+        uar_list.append(df_uar)
+    df = pd.concat(uar_list, keys=[d.name for d in dirs],
+                   names=['corpus', 'classifier', 'config']).unstack(0)
+    print("Data table:")
+    print(df)
+    print()
+    print("Mean across classifiers:")
+    print(df.mean(level=0))
+    print()
+    print("Mean across configs:")
+    print(df.mean(level=1))
+    print()
+    print("Max across configs:")
+    print(df.max(level=0))
+    print()
+    print("Max across configs:")
+    print(df.max(level=1))
+
+    if args.output:
+        df.to_latex(Path(args.output) / 'combined.tex')
+        df.mean(level=0).to_latex(Path(args.output) / 'mean_clf.tex')
+        df.mean(level=1).to_latex(Path(args.output) / 'mean_config.tex')
+        df.max(level=0).to_latex(Path(args.output) / 'max_clf.tex')
+        df.max(level=1).to_latex(Path(args.output) / 'max_config.tex')
+    if args.show:
+        plt.show()
 
 
 if __name__ == "__main__":
