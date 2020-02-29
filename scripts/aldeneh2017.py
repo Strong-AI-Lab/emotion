b58d8459393fd7154ab5dc8b6b517c93d83249f0
Aaron Keesing <agkphysics@gmail.com>
2020-02-29 15:16:03 +1300

    Move for loops outside functions. Switch to padded batching function to reduce memory usage for similar performance.

--- scripts/aldeneh2017.py
+++ scripts/aldeneh2017.py
@@ -6,8 +6,8 @@ Signal Processing (ICASSP), 2017, pp. 2741–2745,
 doi: 10.1109/ICASSP.2017.7952655
 """
 
-import os
 from functools import partial
+from pathlib import Path
 
 import numpy as np
 import tensorflow as tf
@@ -19,7 +19,7 @@ from tensorflow import keras
 from python.classification import (PrecomputedSVC, SKLearnClassifier,
                                    TFClassifier, print_results, test_model)
 from python.dataset import FrameDataset, UtteranceDataset
-from python.tensorflow import BatchedFrameSequence, BatchedSequence
+from python.tensorflow import BatchedSequence
 
 RESULTS_DIR = 'results/aldeneh2017'
 
@@ -57,24 +57,28 @@ def get_conv_model(n_features, n_classes, n_filters=128, kernel_size=8):
 
 def get_full_model(n_features, n_classes):
     inputs = keras.layers.Input(shape=(None, n_features), name='input')
-    x = keras.layers.Conv1D(384, 8, activation='relu',
-                            kernel_initializer='he_normal',
-                            name='conv8')(inputs)
+    x = keras.layers.Conv1D(
+        384, 8, activation='relu', kernel_initializer='he_normal',
+        name='conv8'
+    )(inputs)
     c1 = keras.layers.GlobalMaxPool1D(name='maxpool_1')(x)
 
-    x = keras.layers.Conv1D(384, 16, activation='relu',
-                            kernel_initializer='he_normal',
-                            name='conv16')(inputs)
+    x = keras.layers.Conv1D(
+        384, 16, activation='relu', kernel_initializer='he_normal',
+        name='conv16'
+    )(inputs)
     c2 = keras.layers.GlobalMaxPool1D(name='maxpool_2')(x)
 
-    x = keras.layers.Conv1D(384, 32, activation='relu',
-                            kernel_initializer='he_normal',
-                            name='conv32')(inputs)
+    x = keras.layers.Conv1D(
+        384, 32, activation='relu', kernel_initializer='he_normal',
+        name='conv32'
+    )(inputs)
     c3 = keras.layers.GlobalMaxPool1D(name='maxpool_3')(x)
 
-    x = keras.layers.Conv1D(384, 64, activation='relu',
-                            kernel_initializer='he_normal',
-                            name='conv64')(inputs)
+    x = keras.layers.Conv1D(
+        384, 64, activation='relu', kernel_initializer='he_normal',
+        name='conv64'
+    )(inputs)
     c4 = keras.layers.GlobalMaxPool1D(name='maxpool_4')(x)
 
     x = keras.layers.Concatenate(name='concatenate')([c1, c2, c3, c4])
@@ -120,162 +124,172 @@ def get_tf_dataset(x: np.ndarray, y: np.ndarray, shuffle=True, batch_size=50):
     return data.map(ragged_to_dense)
 
 
-def test_svm_models():
+def test_svm_models(dataset, config):
     param_grid = ParameterGrid({
         'C': 2.0**np.arange(0, 13, 2),
         'gamma': 2.0**np.arange(-15, -2, 2)
     })
 
+    df = test_model(
+        SKLearnClassifier(partial(PrecomputedSVC, kernel='rbf',
+                                  class_weight='balanced')),
+        dataset,
+        reps=1,
+        splitter=LeaveOneGroupOut(),
+        param_grid=param_grid,
+        cv_score_fn=partial(recall_score, average='macro')
+    )
+
+    print_results(df)
+    output_dir = Path(RESULTS_DIR) / dataset.corpus
+    output_dir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(output_dir / '{}.csv'.format(config))
+
+
+def test_dense_model(dataset, config='logmel_func'):
+    model = get_dense_model(480, 4)
+    model.summary()
+    del model
+    keras.backend.clear_session()
+
+    class_weight = ((dataset.n_instances / dataset.n_classes)
+                    / np.bincount(dataset.y.astype(np.int)))
+    class_weight = dict(zip(range(dataset.n_classes), class_weight))
+
+    df = test_model(
+        TFClassifier(partial(get_dense_model, dataset.n_features,
+                             dataset.n_classes)),
+        dataset,
+        reps=1,
+        splitter=LeaveOneGroupOut(),
+        n_epochs=50,
+        class_weight=class_weight,
+        data_fn=partial(BatchedSequence, batch_size=50),
+        callbacks=callbacks_fn(),
+        optimizer=optimizer_fn(),
+        verbose=False
+    )
+
+    print_results(df)
+    output_dir = Path(RESULTS_DIR) / dataset.corpus
+    output_dir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(output_dir / '{}.csv'.format(config))
+
+
+def test_conv_models(dataset, config='logmel'):
+    model = get_conv_model(40, dataset.n_classes)
+    model.summary()
+    del model
+    keras.backend.clear_session()
+
+    class_weight = ((dataset.n_instances / dataset.n_classes)
+                    / np.bincount(dataset.y.astype(np.int)))
+    class_weight = dict(zip(range(dataset.n_classes), class_weight))
+
+    for n_filters, kernel_size in [(384, 8), (288, 16), (208, 32), (128, 64),
+                                   (80, 128)]:
+        print("(n_filters, kernel_size) = ({}, {})".format(n_filters,
+                                                           kernel_size))
+        df = test_model(
+            TFClassifier(partial(
+                get_conv_model, dataset.n_features, dataset.n_classes,
+                n_filters=n_filters, kernel_size=kernel_size
+            )),
+            dataset,
+            reps=1,
+            splitter=LeaveOneGroupOut(),
+            n_epochs=50,
+            class_weight=class_weight,
+            data_fn=get_tf_dataset,
+            callbacks=callbacks_fn(),
+            optimizer=optimizer_fn(),
+            verbose=False
+        )
+
+        print_results(df)
+        output_dir = Path(RESULTS_DIR) / dataset.corpus
+        output_dir.mkdir(parents=True, exist_ok=True)
+        df.to_csv(output_dir / '{}_{}.csv'.format(config, kernel_size))
+
+
+def test_full_model(dataset, config='logmel'):
+    model = get_full_model(40, dataset.n_classes)
+    model.summary()
+    del model
+    keras.backend.clear_session()
+
+    class_weight = ((dataset.n_instances / dataset.n_classes)
+                    / np.bincount(dataset.y.astype(np.int)))
+    class_weight = dict(zip(range(dataset.n_classes), class_weight))
+
+    df = test_model(
+        TFClassifier(partial(get_full_model, dataset.n_features,
+                             dataset.n_classes)),
+        dataset,
+        reps=1,
+        splitter=LeaveOneGroupOut(),
+        n_epochs=50,
+        class_weight=class_weight,
+        data_fn=get_tf_dataset,
+        callbacks=callbacks_fn(),
+        optimizer=optimizer_fn(),
+        verbose=False
+    )
+
+    print_results(df)
+    output_dir = Path(RESULTS_DIR) / dataset.corpus
+    output_dir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(output_dir / '{}_full.csv'.format(config))
+
+
+def main():
+    tf.get_logger().setLevel(40)  # ERROR level
+    for gpu in tf.config.list_physical_devices('GPU'):
+        tf.config.experimental.set_memory_growth(gpu, True)
+
     for corpus in ['iemocap', 'msp-improv']:
         for config in ['IS09_emotion', 'IS13_IS09_func', 'GeMAPSv01a',
                        'eGeMAPSv01a']:
-            print(config)
+            print(corpus, config)
             dataset = UtteranceDataset(
                 '{}/output/{}.arff'.format(corpus, config),
                 normaliser=StandardScaler(),
                 normalise_method='speaker'
             )
             print()
-
-            df = test_model(
-                SKLearnClassifier(partial(PrecomputedSVC, kernel='rbf',
-                                          class_weight='balanced')),
-                dataset,
-                reps=1,
-                splitter=LeaveOneGroupOut(),
-                param_grid=param_grid,
-                cv_score_fn=partial(recall_score, average='macro')
-            )
-
-            print_results(df)
-            df.to_csv(os.path.join(RESULTS_DIR, corpus,
-                                   '{}.csv'.format(config)))
-
-
-def test_dense_model():
-    model = get_dense_model(480, 4)
-    model.summary()
-    keras.backend.clear_session()
-    del model
+            try:
+                test_svm_models(dataset, config)
+            except Exception:
+                pass
 
     for corpus in ['iemocap', 'msp-improv']:
-        print("logmel_IS09_func")
+        print(corpus, "logmel_IS09_func")
         dataset = UtteranceDataset('{}/output/logmel_IS09_func.arff'.format(
             corpus), normaliser=StandardScaler(), normalise_method='speaker')
         print()
-
-        class_weight = ((dataset.n_instances / dataset.n_classes)
-                        / np.bincount(dataset.y.astype(np.int)))
-        class_weight = dict(zip(range(dataset.n_classes), class_weight))
-
-        df = test_model(
-            TFClassifier(partial(get_dense_model, dataset.n_features,
-                                 dataset.n_classes)),
-            dataset,
-            reps=1,
-            splitter=LeaveOneGroupOut(),
-            n_epochs=50,
-            class_weight=class_weight,
-            data_fn=get_tf_dataset,
-            callbacks=callbacks_fn(),
-            optimizer=optimizer_fn(),
-            verbose=False
-        )
-
-        print_results(df)
-        df.to_csv(os.path.join(RESULTS_DIR, corpus, 'logmel_func.csv'))
-
-
-def test_conv_models():
-    model = get_conv_model(40, 4)
-    model.summary()
-    keras.backend.clear_session()
-    del model
+        try:
+            test_dense_model(dataset, 'logmel_IS09_func')
+        except Exception:
+            pass
 
     for corpus in ['iemocap', 'msp-improv']:
-        print("")
+        print(corpus, "logmel")
         dataset = FrameDataset('{}/output/logmel.arff.bin'.format(corpus),
                                normaliser=StandardScaler(),
                                normalise_method='speaker')
         dataset.pad_arrays(32)
         print()
+        try:
+            test_conv_models(dataset, 'logmel')
+        except Exception:
+            pass
 
-        class_weight = ((dataset.n_instances / dataset.n_classes)
-                        / np.bincount(dataset.y.astype(np.int)))
-        class_weight = dict(zip(range(dataset.n_classes), class_weight))
-
-        for n_filters, kernel_size in [(384, 8), (288, 16), (208, 32),
-                                       (128, 64), (80, 128)]:
-            print("(n_filters, kernel_size) = ({}, {})".format(n_filters,
-                                                               kernel_size))
-            df = test_model(
-                TFClassifier(partial(
-                    get_conv_model, dataset.n_features, dataset.n_classes,
-                    n_filters=n_filters, kernel_size=kernel_size
-                )),
-                dataset,
-                reps=1,
-                splitter=LeaveOneGroupOut(),
-                n_epochs=50,
-                class_weight=class_weight,
-                data_fn=partial(BatchedFrameSequence, batch_size=50),
-                callbacks=callbacks_fn(),
-                optimizer=optimizer_fn(),
-                verbose=False
-            )
-
-            print_results(df)
-            df.to_csv(os.path.join(RESULTS_DIR, corpus,
-                                   'logmel_{}.csv'.format(kernel_size)))
-
-
-def test_full_model():
-    model = get_full_model(40, 4)
-    model.summary()
-    keras.backend.clear_session()
-    del model
-
-    for corpus in ['iemocap', 'msp-improv']:
-        dataset = FrameDataset('{}/output/logmel.arff.bin'.format(corpus),
-                               normaliser=StandardScaler(),
-                               normalise_method='speaker')
-        dataset.pad_arrays(32)
+        print(corpus, "logmel_full")
+        try:
+            test_full_model(dataset, 'logmel')
+        except Exception:
+            pass
         print()
-
-        class_weight = ((dataset.n_instances / dataset.n_classes)
-                        / np.bincount(dataset.y.astype(np.int)))
-        class_weight = dict(zip(range(dataset.n_classes), class_weight))
-
-        df = test_model(
-            TFClassifier(partial(get_full_model, dataset.n_features,
-                                 dataset.n_classes)),
-            dataset,
-            reps=1,
-            splitter=LeaveOneGroupOut(),
-            n_epochs=50,
-            class_weight=class_weight,
-            data_fn=get_tf_dataset,
-            callbacks=callbacks_fn(),
-            optimizer=optimizer_fn(),
-            verbose=False
-        )
-
-        print_results(df)
-        df.to_csv(os.path.join(RESULTS_DIR, corpus, 'logmel_full.csv'))
-
-
-def main():
-    os.makedirs(os.path.join(RESULTS_DIR, 'iemocap'), exist_ok=True)
-    os.makedirs(os.path.join(RESULTS_DIR, 'msp-improv'), exist_ok=True)
-
-    tf.get_logger().setLevel(40)  # ERROR level
-    for gpu in tf.config.list_physical_devices('GPU'):
-        tf.config.experimental.set_memory_growth(gpu, True)
-
-    test_svm_models()
-    test_dense_model()
-    test_conv_models()
-    test_full_model()
 
 
 if __name__ == "__main__":
