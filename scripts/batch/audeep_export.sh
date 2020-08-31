17ce4e1f2ef3834b87ce96a408bf93a23aa189bc
Aaron Keesing <agkphysics@gmail.com>
2020-08-31 13:52:18 +1200

    Change script names and directories to be more informative.

--- scripts/batch/audeep_export.sh
+++ scripts/batch/audeep_export.sh
@@ -6,5 +6,5 @@
 for corpus in cafe crema-d demos emodb emofilm enterface iemocap jl msp-improv portuguese ravdess savee shemo smartkom tess; do
     echo "Generating $corpus"
     scripts/training/audeep_generate.sh $corpus
-    python scripts/utils/reset_audeep_corpus.py datasets/${corpus}/output/audeep.nc $corpus
+    python scripts/utils/audeep_corpus_name.py datasets/${corpus}/output/audeep.nc $corpus
 done
