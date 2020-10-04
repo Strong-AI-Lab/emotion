e34c16a107aa3427c2c96ffee2eed9602f08ef3a
Aaron Keesing <agkphysics@gmail.com>
2020-10-04 20:28:31 +1300

    Add scripts to generate raw audio dataset and run parallel GPU jobs.

---
+++ scripts/preprocessing/create_raw_dataset.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""Creates a NetCDF dataset containing the raw audio and labels."""
+
+import argparse
+import sys
+from pathlib import Path
+
+import numpy as np
+import soundfile
+from emotion_recognition.dataset import write_netcdf_dataset
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=Path, required=True)
+    parser.add_argument('--corpus', type=str, required=True)
+    parser.add_argument('--annotations', type=Path, required=True)
+    parser.add_argument('--output', type=Path, required=True)
+    args = parser.parse_args()
+
+    with open(args.input) as fid:
+        filenames = [line.strip() for line in fid]
+
+    print("Processing {} audio files.".format(len(filenames)))
+    audio_arr = []
+    for filename in filenames:
+        audio, sr = soundfile.read(filename, always_2d=True, dtype=np.float32)
+        if sr != 16000:
+            sys.stderr.write(
+                "Sample rate of {} != 16000, skipping.\n".format(filename))
+            continue
+        audio = np.mean(audio, axis=1)
+        audio = np.expand_dims(audio, axis=1)
+        audio_arr.append(audio)
+    slices = [len(x) for x in audio_arr]
+    audio_arr = np.concatenate(audio_arr)
+    print("Num samples:")
+    print("\ttotal: {}".format(sum(slices)))
+    print("\tmin: {}".format(min(slices)))
+    print("\tmax: {}".format(max(slices)))
+    print("\tmean: {}".format(np.mean(slices)))
+    print("\tstd: {}".format(np.std(slices)))
+
+    names = [Path(f).stem for f in filenames]
+    write_netcdf_dataset(
+        args.output, corpus=args.corpus, names=names, slices=slices,
+        features=audio_arr, annotation_path=args.annotations
+    )
+    print("Wrote NetCDF4 dataset to {}.".format(args.output))
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/training/parallel_jobs.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import queue
+import subprocess
+import sys
+import threading
+from pathlib import Path
+
+q = queue.Queue()
+
+
+def worker(gpu: int):
+    env = os.environ.copy()
+    env.update({'CUDA_VISIBLE_DEVICES': str(gpu)})
+    while not q.empty():
+        cmd = q.get()
+        print("Executing command on GPU_{}: {}".format(gpu, cmd))
+        proc = subprocess.Popen(cmd, env=env, shell=True, text=True, bufsize=1,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        with proc.stdout:
+            for line in iter(proc.stdout.readline, ''):
+                sys.stdout.write("GPU_{}: {}".format(gpu, line))
+        proc.wait()
+        q.task_done()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input', type=Path, required=True,
+        help="File containing train commands that can each run on a single "
+             "GPU."
+    )
+    parser.add_argument('--gpus', type=int, default=2, help="Number of GPUs.")
+    args = parser.parse_args()
+
+    with open(args.input) as fid:
+        for line in fid:
+            line = line.strip()
+            q.put(line)
+            print("Command {}".format(line))
+
+    threads = [threading.Thread(target=worker, args=(i,))
+               for i in range(args.gpus)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+
+if __name__ == "__main__":
+    main()
