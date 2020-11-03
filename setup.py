eb51fde3182def27399f712542cb4d876a1fc1e2
Aaron Keesing <akee511@aucklanduni.ac.nz>
2020-11-03 17:24:47 +1300

    Use new Python build system configuration.

--- setup.py
+++ setup.py
@@ -1,11 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='emotion_recognition',
-    version='0.1.0',
-    description="Tools for training and testing emotion recognition models and datasets.",
-    author='Aaron Keesing',
-    url='https://github.com/agkphysics/emotion',
-    python_requires='>=3.6',
-    packages=find_packages(include='emotion_recognition')
-)
+import setuptools
+setuptools.setup()
