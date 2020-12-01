faa7390c8c11bd9fd413b677f2573f1f7929bf0d
Aaron Keesing <akee511@aucklanduni.ac.nz>
2020-12-01 17:09:00 +1300

    Add corpus metadata validation script.

---
+++ scripts/utils/validate_corpus_metadata.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+from pathlib import Path
+
+import jsonschema
+import requests
+
+SCHEMA_URI = 'https://agkphysics.github.io/schemas/emotional-speech-data/'
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', type=Path)
+    parser.add_argument('--version', type=str, default='latest')
+    args = parser.parse_args()
+
+    schema_uri = SCHEMA_URI + args.version
+    schema = json.loads(requests.get(schema_uri).content)
+
+    with open(args.input) as fid:
+        data = json.load(fid)
+    jsonschema.validate(data, schema)
+    print("Metadata successfully validated.")
+
+
+if __name__ == "__main__":
+    main()
