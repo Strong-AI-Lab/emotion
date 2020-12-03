5c0203ded49c3300f6135bab5b493eb16536da91
Aaron Keesing <akee511@aucklanduni.ac.nz>
2020-12-04 12:18:00 +1300

    Correct spectrogram output filename.

--- papers/alta2020/run_preprocessing.sh
+++ papers/alta2020/run_preprocessing.sh
@@ -12,5 +12,5 @@ python scripts/preprocessing/opensmile.py --corpus $1 --config third_party/opens
 python scripts/preprocessing/opensmile.py --corpus $1 --config third_party/opensmile/conf/logmel.conf --annotations datasets/$1/labels.csv --input datasets/$1/files.txt --output output/$1/logmel_240.nc -nMelBands 240
 
 # Fixed-size clipped log-mel spectrograms
-python scripts/preprocessing/extract_spectrograms.py --corpus $1 --length 5 --clip 60 --window_size 0.025 --window_shift 0.010 --pre_emphasis 0.97 --labels datasets/$1/labels.csv --input datasets/$1/files.txt --netcdf output/$1/spectrograms_40.nc --mel_bands 40
-python scripts/preprocessing/extract_spectrograms.py --corpus $1 --length 5 --clip 60 --window_size 0.025 --window_shift 0.010 --pre_emphasis 0.97 --labels datasets/$1/labels.csv --input datasets/$1/files.txt --netcdf output/$1/spectrograms_240.nc --mel_bands 240
+python scripts/preprocessing/extract_spectrograms.py --corpus $1 --length 5 --clip 60 --window_size 0.025 --window_shift 0.010 --pre_emphasis 0.97 --labels datasets/$1/labels.csv --input datasets/$1/files.txt --netcdf output/$1/spectrograms-0.025-0.010-40-60.nc --mel_bands 40
+python scripts/preprocessing/extract_spectrograms.py --corpus $1 --length 5 --clip 60 --window_size 0.025 --window_shift 0.010 --pre_emphasis 0.97 --labels datasets/$1/labels.csv --input datasets/$1/files.txt --netcdf output/$1/spectrograms-0.025-0.010-240-60.nc --mel_bands 240
