d5aa9a386cc130c72942aa5f3081e610163224d7
Aaron Keesing <akee511@aucklanduni.ac.nz>
2020-12-11 15:38:00 +1300

    Add instance feature viewing script.

---
+++ scripts/utils/view_instance.py
@@ -0,0 +1,47 @@
+"""Visualise 2D features for a single instance, or a collection of
+feature vectors for multiple instances.
+"""
+
+import argparse
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+import numpy as np
+from emotion_recognition.dataset import Dataset
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', type=Path)
+    parser.add_argument('--instance', type=str, default='2')
+    args = parser.parse_args()
+
+    dataset = Dataset(args.input)
+    if args.instance.isdigit():
+        instance = int(args.instance)
+    else:
+        idx = args.instance.find(':')
+        if idx != -1:
+            start = args.instance[:idx]
+            end = args.instance[idx + 1:]
+            if start.isdigit():
+                start = int(start)
+            if end.isdigit():
+                end = int(end)
+            instance = slice(start, end)
+        else:
+            instance = dataset.names.index(args.instance)
+    arr = dataset.x[instance]
+    if arr.dtype == object:
+        pass
+    if len(arr.shape) == 1:
+        arr = np.expand_dims(arr, 0)
+
+    plt.figure()
+    plt.imshow(arr, aspect='equal', origin='upper',
+               interpolation='nearest')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
