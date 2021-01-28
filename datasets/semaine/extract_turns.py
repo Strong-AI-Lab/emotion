39247b99e7f6c3fdf72eb8635537bb110941deb9
Aaron Keesing <akee511@aucklanduni.ac.nz>
2021-01-29 01:01:37 +1300

    Update SEMAINE processing scripts.

--- datasets/semaine/extract_turns.py
+++ datasets/semaine/extract_turns.py
@@ -1,39 +1,30 @@
-import argparse
 import re
 from pathlib import Path
 
+import click
 import numpy as np
+import pandas as pd
 import soundfile
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_dir", type=Path, default="combined")
-    parser.add_argument("annotation_file", type=Path, default="annot.txt")
-    args = parser.parse_args()
+@click.command()
+@click.argument('combined', type=Path, default='combined')
+def main(combined: Path):
+    """Splits full recordings into individual turns."""
+    df = pd.DataFrame(columns=['Activation', 'Expectation', 'Power',
+                               'Valence'])
+    for recording_dir in filter(Path.is_dir, combined.glob('*')):
+        print(recording_dir)
 
-    if not args.input_dir.exists():
-        raise FileNotFoundError("Directory doesn't exist: " + args.input_dir)
-
-    annot_file = open(args.annotation_file, 'w')
-
-    recording_dirs = filter(Path.is_dir, args.input_dir.glob('*'))
-    for recording_dir in sorted(recording_dirs):
-        if not recording_dir.is_dir():
-            continue
-
-        operator_audio, _ = soundfile.read(
-            recording_dir / 'operator_audio.wav')
-        user_audio, _ = soundfile.read(recording_dir / 'user_audio.wav')
-        with open(recording_dir / 'emotions.txt') as fid:
-            lines = [l.strip() for l in fid][1:]  # Ignore header line
-            emotion_data = np.array([
-                [float(x) for x in l.split()] for l in lines])
+        operator_audio, _ = soundfile.read(recording_dir / 'operator.wav')
+        user_audio, _ = soundfile.read(recording_dir / 'user.wav')
+        emotions = pd.read_csv(recording_dir / 'emotions.csv', header=0,
+                               index_col=0)
 
         user_turns = {}
         operator_turns = {}
-        for d, f in [(user_turns, 'words_user.txt'),
-                     (operator_turns, 'words_operator.txt')]:
+        for d, f in [(user_turns, 'user.txt'),
+                     (operator_turns, 'operator.txt')]:
             with open(recording_dir / f) as fid:
                 turn = 0
                 for line in fid:
@@ -67,18 +58,16 @@ def main():
 
                 if p == 'u':
                     start_idx, end_idx = np.searchsorted(
-                        emotion_data[:, 0],
-                        [start / 16000, end / 16000]
+                        emotions.index, [start / 16000, end / 16000]
                     )
                     if start_idx != end_idx:
-                        mean_emotions = emotion_data[
-                            start_idx:end_idx, 1:].mean(0)
-                        print(
-                            "{}, A: {:.4f}, E: {:.4f}, P: {:.4f}, V: {:.4f}"
-                            .format(name, *mean_emotions),
-                            file=annot_file
-                        )
-    annot_file.close()
+                        mean_emotions = emotions.iloc[
+                            start_idx:end_idx, :].mean()
+                        df.loc[name, :] = mean_emotions
+
+    df.index.name = 'Name'
+    for c in df.columns:
+        df[c].to_csv('{}.csv'.format(c.lower()))
 
 
 if __name__ == "__main__":
--- datasets/semaine/process.py
+++ datasets/semaine/process.py
@@ -1,42 +1,47 @@
-import argparse
 import re
 from pathlib import Path
-from xml.etree import ElementTree as et
+from xml.etree import ElementTree as ET
 
+import click
 import numpy as np
+import pandas as pd
 import soundfile
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("session_dir", type=Path, default="Sessions")
-    parser.add_argument("output_dir", type=Path, default="combined")
-    args = parser.parse_args()
+@click.command()
+@click.argument('sess_dir', type=Path, default='Sessions')
+@click.argument('output', type=Path, default='combined')
+def main(sess_dir: Path, output: Path):
+    """Processes all sessions in the SEMAINE corpus from the SESS_DIR
+    directory and puts combined audio/transcripts/ratings into the
+    OUTPUT directory.
 
+    By default SESS_DIR is assumed to be `Sessions/`.
+    """
     recordings = {}
-    session_dirs = args.session_dir.glob('*')
+    session_dirs = sess_dir.glob('*')
     for session_dir in session_dirs:
         session_id = int(session_dir.stem)
 
         session_file = session_dir / 'session.xml'
-        session_xml = et.parse(session_file).getroot()
+        session_xml = ET.parse(session_file).getroot()
         recording = int(session_xml.attrib['recording'])
         character = session_xml.attrib['character']
 
-        files = session_dir.glob('*')
-        word_annotations_user = next((f for f in files if re.search(
-            r'wordLevel_alignedTranscript.*_user', f)), None)
-        word_annotations_operator = next((f for f in files if re.search(
-            r'wordLevel_alignedTranscript.*_operator', f)), None)
-        aligned_transcript = next((f for f in files if re.search(
-            r'[^_]alignedTranscript_[0-9]+.*\.txt', f)), None)
+        files = list(session_dir.glob('*'))
+        words_user_path = next((f for f in files if re.search(
+            r'wordLevel_alignedTranscript.*_user', f.name)), None)
+        words_operator_path = next((f for f in files if re.search(
+            r'wordLevel_alignedTranscript.*_operator', f.name)), None)
+        transcript_path = next((f for f in files if re.search(
+            r'^alignedTranscript_[0-9]+.*\.txt', f.name)), None)
         feeltraces = [f for f in files if re.search(
-            r'[AR][0-9].*\.txt', f)]
+            r'[AR][0-9].*\.txt', f.name)]
         annotations = {}
         for trace in feeltraces:
             match = re.search(
                 r'[AR]([0-9]+)[RS]([0-9]+)TUC(Ob|Po|Pr|Sp)2?D([AEPV])\.txt',
-                trace
+                trace.name
             )
             if match:
                 rater = int(match.group(1))
@@ -45,19 +50,19 @@ def main():
                 if emotion not in annotations:
                     annotations[emotion] = {}
                 annotations[emotion][rater] = trace
-        operator_audio = next((f for f in files if re.search(
-            r'Operator HeadMounted.*\.wav', f)), None)
-        user_audio = next((f for f in files if re.search(
-            r'User HeadMounted.*\.wav', f)), None)
+        operator_audio_path = next((f for f in files if re.search(
+            r'Operator HeadMounted.*\.wav', f.name)), None)
+        user_audio_path = next((f for f in files if re.search(
+            r'User HeadMounted.*\.wav', f.name)), None)
         if recording not in recordings:
             recordings[recording] = []
         session_info = (session_id,
                         character,
-                        operator_audio,
-                        word_annotations_operator,
-                        user_audio,
-                        word_annotations_user,
-                        aligned_transcript,
+                        operator_audio_path,
+                        words_operator_path,
+                        user_audio_path,
+                        words_user_path,
+                        transcript_path,
                         annotations)
         recordings[recording].append(session_info)
 
@@ -69,28 +74,24 @@ def main():
         concat_words_operator = ''
         concat_transcript = ''
         emotion_data = {}
-        sessions.sort(key=lambda x: x[0])
+        sessions = sorted(sessions, key=lambda x: x[0])
         print("Recording {}:".format(recording))
         for (session_id,
              character,
-             operator_audio,
-             word_annotations_operator,
-             user_audio,
-             word_annotations_user,
-             aligned_transcript,
+             operator_audio_path,
+             words_operator_path,
+             user_audio_path,
+             words_user_path,
+             transcript_path,
              annotations) in sessions:
             if (character.lower() in ['beginning', 'end', 'forbidden']
-                    or not word_annotations_user):
+                    or not words_user_path):
+                print("\tSession {} skipped due to having no transcript."
+                      .format(session_id))
+                print()
                 continue
 
-            audio, _ = soundfile.read(user_audio)
-            concat_user_audio.append(audio.data)
-            audio, _ = soundfile.read(operator_audio)
-            concat_operator_audio.append(audio.data)
-
-            print('\t', word_annotations_operator)
-            print('\t', word_annotations_user)
-            print('\t', aligned_transcript)
+            print("\tSession {}:".format(session_id))
 
             for emotion in annotations:
                 rater_data = []
@@ -99,7 +100,7 @@ def main():
                     rater_data.append(
                         np.fromfile(trace, sep=' ').reshape((-1, 2)))
 
-                # Clip rows to size of smallest array across raters
+                # Truncate time to size of smallest array across raters
                 smallest = np.argmin([x.shape[0] for x in rater_data])
                 max_size = np.max([x.shape[0] for x in rater_data])
                 num_indices = rater_data[smallest].shape[0]
@@ -109,7 +110,7 @@ def main():
                 min_size = num_indices
                 print('\t', emotion, len(rater_data), rater_data[0].shape)
                 if max_size - min_size > 2:
-                    print("\t\t WARNING: Raters difference: ",
+                    print("\t\t WARNING: Rating length difference: ",
                           max_size - min_size)
                 mean_values = np.stack(
                     [x[:, 1] for x in rater_data], axis=0).mean(0)
@@ -123,9 +124,9 @@ def main():
                     emotion_data[emotion] = np.concatenate(
                         [emotion_data[emotion], mean_raters], axis=0)
 
-            # Clip rows to size of smallest array across emotions
-            smallest_emotion = min(
-                emotion_data, key=lambda e: emotion_data[e].shape[0])
+            # Truncate time to size of smallest array across emotions
+            smallest_emotion = min(emotion_data,
+                                   key=lambda e: emotion_data[e].shape[0])
             max_size = max([emotion_data[e].shape[0] for e in emotion_data])
             num_indices = emotion_data[smallest_emotion].shape[0]
             for e in emotion_data:
@@ -133,24 +134,29 @@ def main():
 
             min_size = num_indices
             if max_size - min_size > 2:
-                print("\t WARNING: Emotions difference: ", max_size - min_size)
+                print("\t WARNING: Annotation length difference: {}".format(
+                    max_size - min_size))
 
-            if aligned_transcript:
-                with open(aligned_transcript) as fid:
+            if transcript_path:
+                print('\t', transcript_path.name)
+                with open(transcript_path) as fid:
                     concat_transcript += fid.read()
-            if word_annotations_user:
-                with open(word_annotations_user) as fid:
-                    for line in fid:
-                        m = re.search(
-                            r'([0-9]+) ([0-9]+) ([A-Z\'"?.<>]+)', line)
-                        if m:
-                            line = '{} {} {}\n'.format(
-                                int(m.group(1)) + duration,
-                                int(m.group(2)) + duration,
-                                m.group(3))
-                        concat_words_user += line
-            if word_annotations_operator:
-                with open(word_annotations_operator) as fid:
+
+            print('\t', words_user_path.name)
+            with open(words_user_path) as fid:
+                for line in fid:
+                    m = re.search(
+                        r'([0-9]+) ([0-9]+) ([A-Z\'"?.<>]+)', line)
+                    if m:
+                        line = '{} {} {}\n'.format(
+                            int(m.group(1)) + duration,
+                            int(m.group(2)) + duration,
+                            m.group(3))
+                    concat_words_user += line
+
+            if words_operator_path:
+                print('\t', words_operator_path.name)
+                with open(words_operator_path) as fid:
                     for line in fid:
                         m = re.search(
                             r'([0-9]+) ([0-9]+) ([A-Z\'"?.<>]+)', line)
@@ -160,37 +166,38 @@ def main():
                                 int(m.group(2)) + duration,
                                 m.group(3))
                         concat_words_operator += line
-            duration += int(1000 * len(audio) / 16000)
 
-        if len(emotion_data.keys()) < 4:
+            audio, _ = soundfile.read(user_audio_path)
+            concat_user_audio.append(audio)
+            audio, _ = soundfile.read(operator_audio_path)
+            concat_operator_audio.append(audio)
+            duration += int(1000 * len(audio) / 16000)
+            print()
+
+        if len(emotion_data) < 4:
             continue
 
-        concat_user_audio = np.array(concat_user_audio)
-        concat_operator_audio = np.array(concat_operator_audio)
+        times = emotion_data['Valence'][:, 0]
+        emotion_data = {e: x[:, 1] for e, x in emotion_data.items()}
 
-        output_dir = args.output_dir / str(recording)
-        output_dir.mkdir(exist_ok=True)
-        soundfile.write(output_dir / 'user_audio.wav', concat_user_audio,
+        concat_user_audio = np.concatenate(concat_user_audio)
+        concat_operator_audio = np.concatenate(concat_operator_audio)
+
+        output_dir = output / str(recording)
+        output_dir.mkdir(exist_ok=True, parents=True)
+        soundfile.write(output_dir / 'user.wav', concat_user_audio,
                         samplerate=16000)
-        soundfile.write(output_dir / 'operator_audio.wav',
-                        concat_operator_audio, samplerate=16000)
+        soundfile.write(output_dir / 'operator.wav', concat_operator_audio,
+                        samplerate=16000)
         with open(output_dir / 'transcript.txt', 'w') as fid:
             fid.write(concat_transcript)
-        with open(output_dir / 'words_user.txt', 'w') as fid:
+        with open(output_dir / 'user.txt', 'w') as fid:
             fid.write(concat_words_user)
-        with open(output_dir / 'words_operator.txt', 'w') as fid:
+        with open(output_dir / 'operator.txt', 'w') as fid:
             fid.write(concat_words_operator)
-        with open(output_dir / 'emotions.txt', 'w') as fid:
-            fid.write("Time")
-            sorted_emotions = sorted(emotion_data.keys())  # For consistency
-            for emotion in sorted_emotions:
-                fid.write(' ' + emotion)
-            fid.write('\n')
-            for i in range(emotion_data['Valence'].shape[0]):
-                fid.write('{:.2f}'.format(emotion_data['Valence'][i, 0]))
-                for emotion in sorted_emotions:
-                    fid.write(' {:.4f}'.format(emotion_data[emotion][i, 1]))
-                fid.write('\n')
+
+        df = pd.DataFrame(emotion_data, index=pd.Index(times, name='Time'))
+        df.to_csv(output_dir / 'emotions.csv')
 
 
 if __name__ == "__main__":
