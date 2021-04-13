664958053be574cb2fd14759b8bd8303c4fc7081
Aaron Keesing <akee511@aucklanduni.ac.nz>
2021-04-13 15:43:47 +1200

    Update annotation_stats.py for pairwise plots.
    annotation_stats.py now plots pairs of variables with an appropriate
    plot. The reference variable is the one first listed on the commandline.

--- scripts/utils/annotation_stats.py
+++ scripts/utils/annotation_stats.py
@@ -1,54 +1,87 @@
-from collections import Counter
 from pathlib import Path
+from typing import List, Tuple
 
 import click
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-from emorec.dataset import parse_annotations
+from emorec.dataset import get_audio_paths
 from emorec.utils import PathlibPath
 
 
 @click.command()
-@click.argument("input", type=PathlibPath(exists=True, dir_okay=False))
+@click.argument("input", type=PathlibPath(exists=True, dir_okay=False), nargs=-1)
 @click.option("--plot", is_flag=True, help="Plot histogram/bar chart.")
-def main(input: Path, plot: bool):
-    """Calculate statistics from annotations in INPUT."""
-    _df = pd.read_csv(input, header=0, nrows=1)
-    if plot:
-        fig, ax = plt.subplots()
-        ax.set_title(f"Distribution of {input.stem}")
-        ax.set_xlabel(input.stem)
-        ax.set_ylabel("Count")
+@click.option(
+    "--files",
+    type=PathlibPath(exists=True, dir_okay=False),
+    help="File with names to include for statistics.",
+)
+def main(input: Tuple[Path], plot: bool, files: Path):
+    """Calculate statistics from annotations in INPUT(s). If multiple
+    INPUTs are given, additional statistics for the other INPUTs are
+    shown for each level of INPUT.
+    """
 
-    if _df.columns[1].lower() in ["speaker", "label"]:
-        dict_ = parse_annotations(input, dtype=str)
-        counts = Counter(dict_.values())
-        print(f"{len(counts)} distinct values of {input.stem}:")
-        for k, v in counts.most_common():
-            print(f"{k}: {v}")
+    dfs: List[pd.DataFrame] = []
+    for file in input:
+        df = pd.read_csv(file, header=0, converters={0: str}).set_index("name")
+        if files:
+            names = {Path(x).stem for x in get_audio_paths(files)}
+            df = df[df.index.isin(names)]
+        dfs.append(df)
+        col = df[df.columns[0]]
+        print(f"Statistics for {col.name}:")
+        print("===========================")
+        print(col.describe().to_string())
+        print()
+        if col.dtype == object:
+            counts = col.value_counts()
+            print("Value counts:")
+            print(counts.to_string())
+            print()
+
         if plot:
-            ax.bar(counts.keys(), counts.values())
-            ax.set_xticklabels(
-                counts.keys(),
-                rotation=30,
-                ha="right",
-                fontsize="small",
-                rotation_mode="anchor",
-            )
+            fig, ax = plt.subplots()
+            ax.set_title(f"Distribution of {file.stem}")
+            if col.dtype == object:
+                col.value_counts().plot(kind="bar", ax=ax)
+            else:
+                col.plot(kind="hist", ylabel=None, ax=ax)
             fig.tight_layout()
-    else:
-        dict_ = parse_annotations(input)
-        values = np.array(list(dict_.values()))
-        print(f"Min: {np.min(values)}")
-        print(f"Mean: {np.mean(values)}")
-        print(f"Median: {np.median(values)}")
-        print(f"Max: {np.max(values)}")
-        print(f"Std: {np.std(values)}")
-        if plot:
-            ax.hist(values)
-    plt.show()
+
+    print("Pairwise tables:")
+    if len(input) > 1:
+        joined = dfs[0]
+        refcol = joined.columns[0]
+        for df in dfs[1:]:
+            joined = pd.merge(joined, df, left_index=True, right_index=True)
+        for col in joined.columns[1:]:
+            print(f"For column '{col}'")
+            if plot:
+                fig, ax = plt.subplots()
+                ax.set_title(f"Distribution of {col} per {refcol}")
+
+            if joined[refcol].dtype == object:
+                if joined[col].dtype == object:
+                    table = joined.groupby([refcol, col]).size().unstack(fill_value=0)
+                    print(table.to_string())
+                    if plot:
+                        table.plot(kind="bar", stacked=True, ax=ax)
+                else:
+                    print(joined.groupby([refcol])[col].describe())
+                    if plot:
+                        table = joined.groupby([refcol])[col].hist(
+                            bins=5, alpha=0.3, legend=True, ax=ax
+                        )
+            else:
+                joined.plot(kind="scatter", x=refcol, y=col, ax=ax)
+            print()
+            if plot:
+                fig.tight_layout()
+
+    if plot:
+        plt.show()
 
 
 if __name__ == "__main__":
