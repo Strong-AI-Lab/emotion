4c545edb93110d6b0846e3390e050ac680d7a318
Aaron Keesing <akee511@aucklanduni.ac.nz>
2021-05-07 17:59:06 +1200

    Use toggleable header/label options in convert.py

--- scripts/utils/convert.py
+++ scripts/utils/convert.py
@@ -11,16 +11,18 @@ from emorec.utils import PathlibPath
 @click.argument("output", type=Path)
 @click.option("--corpus", type=str, help="Corpus attribute to set, if required.")
 @click.option(
-    "--header",
-    is_flag=True,
+    "--header/--noheader",
+    default=True,
     help="Input CSV has header/Write output CSV header.",
+    show_default=True,
 )
 @click.option(
-    "--inlabel",
-    is_flag=True,
+    "--label/--nolabel",
+    default=False,
     help="Input has label column.",
+    show_default=True,
 )
-def main(input: Path, output: Path, corpus: str, header: bool, inlabel: bool):
+def main(input: Path, output: Path, corpus: str, header: bool, label: bool):
     """Convert INPUT dataset format to OUTPUT format. Note that no label
     information is written to OUTPUT.
     """
@@ -29,7 +31,7 @@ def main(input: Path, output: Path, corpus: str, header: bool, inlabel: bool):
         raise ValueError("Input format must be different to output.")
 
     print(f"Reading {input}")
-    data = read_features(input, header=header, label=inlabel)
+    data = read_features(input, header=header, label=label)
     if corpus:
         data._corpus = corpus
     output.parent.mkdir(parents=True, exist_ok=True)
