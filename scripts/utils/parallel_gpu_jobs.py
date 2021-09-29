76725aca58d834304454cfcfce98c642c49b27c7
Aaron Keesing <akee511@aucklanduni.ac.nz>
2021-09-29 13:06:34 +1300

    parallel_jobs: multiple files, log failed jobs.

--- scripts/utils/parallel_gpu_jobs.py
+++ scripts/utils/parallel_gpu_jobs.py
@@ -3,59 +3,67 @@ import subprocess
 import sys
 from pathlib import Path
 from queue import Queue
-from threading import Thread
+from threading import Lock, Thread
+from typing import Tuple
 
 import click
 
 from ertk.utils import PathlibPath
 
-q: "Queue[str]" = Queue()
-
-
-def worker(gpu: int):
-    env = os.environ.copy()
-    env.update({"CUDA_VISIBLE_DEVICES": str(gpu), "PYTHONUNBUFFERED": "1"})
-    while not q.empty():
-        cmd = q.get()
-        print(f"Executing command on GPU_{gpu}: {cmd}")
-        proc = subprocess.Popen(
-            cmd,
-            env=env,
-            shell=True,
-            text=True,
-            bufsize=1,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        )
-        if proc.stdout is not None:
-            with proc.stdout:
-                for line in iter(proc.stdout.readline, ""):
-                    sys.stdout.write(f"[GPU{gpu}]: {line}")
-                    sys.stdout.flush()
-        proc.wait()
-        q.task_done()
-
 
 @click.command()
-@click.argument("input", type=PathlibPath(exists=True, dir_okay=False))
+@click.argument("input", type=PathlibPath(exists=True, dir_okay=False), nargs=-1)
 @click.option("--gpus", default="0", help="GPUs to run on.", show_default=True)
-def main(input: Path, gpus: str):
-    """Runs all commands specified in the INPUT file, splitting the work
-    across multiple GPUs such that each command runs solely on whichever
-    GPU is next available.
+@click.option("--failed", type=Path, help="Where to log failed commands.")
+def main(input: Tuple[Path], gpus: str, failed: Path):
+    """Runs all commands specified in the INPUT file(s), splitting the
+    work across multiple GPUs such that each command runs solely on
+    whichever GPU is next available.
 
     Each GPU is given its own thread that reads from a synchronous queue
     and runs the command in a shell with the corresponding
     CUDA_VISIBLE_DEVICES environment variable set appropriately.
     """
+    if len(input) == 0:
+        raise RuntimeError("No input files specified.")
 
-    with open(input) as fid:
-        for line in fid:
-            line = line.strip()
-            if len(line) == 0:
-                continue
-            q.put(line)
-            print(f"Command {line}")
+    q: "Queue[str]" = Queue()
+    file_lock = Lock()
+    fail_file = open(failed, "w")
+
+    def worker(gpu: int):
+        env = os.environ.copy()
+        env.update({"CUDA_VISIBLE_DEVICES": str(gpu), "PYTHONUNBUFFERED": "1"})
+        while not q.empty():
+            cmd = q.get()
+            print(f"Executing command on GPU_{gpu}: {cmd}")
+            proc = subprocess.Popen(
+                cmd,
+                env=env,
+                shell=True,
+                text=True,
+                bufsize=1,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+            if proc.stdout is not None:
+                with proc.stdout:
+                    for line in iter(proc.stdout.readline, ""):
+                        sys.stdout.write(f"[GPU{gpu}]: {line}")
+                        sys.stdout.flush()
+            if proc.wait() != 0:
+                with file_lock:
+                    fail_file.write(cmd + "\n")
+            q.task_done()
+
+    for file in input:
+        with open(file) as fid:
+            for line in fid:
+                line = line.strip()
+                if len(line) == 0:
+                    continue
+                q.put(line)
+                print(f"Command {line}")
 
     try:
         _gpus = [int(gpus)]
--- scripts/utils/parallel_jobs.py
+++ scripts/utils/parallel_jobs.py
@@ -3,41 +3,16 @@ import subprocess
 import sys
 from pathlib import Path
 from queue import Queue
-from threading import Thread
+from threading import Lock, Thread
+from typing import Tuple
 
 import click
 
 from ertk.utils import PathlibPath
 
-q: "Queue[str]" = Queue()
-
-
-def worker(t_id: int):
-    env = os.environ.copy()
-    env.update({"PYTHONUNBUFFERED": "1"})
-    while not q.empty():
-        cmd = q.get()
-        print(f"Executing command on thread {t_id}: {cmd}")
-        proc = subprocess.Popen(
-            cmd,
-            env=env,
-            shell=True,
-            text=True,
-            bufsize=1,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        )
-        if proc.stdout is not None:
-            with proc.stdout:
-                for line in iter(proc.stdout.readline, ""):
-                    sys.stdout.write(f"[T{t_id}]: {line}")
-                    sys.stdout.flush()
-        proc.wait()
-        q.task_done()
-
 
 @click.command()
-@click.argument("input", type=PathlibPath(exists=True, dir_okay=False))
+@click.argument("input", type=PathlibPath(exists=True, dir_okay=False), nargs=-1)
 @click.option(
     "--threads",
     "n_threads",
@@ -46,22 +21,55 @@ def worker(t_id: int):
     help="Number of threads",
     show_default=True,
 )
-def main(input: Path, n_threads: int):
-    """Runs all commands specified in the INPUT file, splitting the work
-    across multiple threads such that each command runs solely on
+@click.option("--failed", type=Path, help="Where to log failed commands.")
+def main(input: Tuple[Path], n_threads: int, failed: Path):
+    """Runs all commands specified in the INPUT file(s), splitting the
+    work across multiple threads such that each command runs solely on
     whichever thread is next available.
 
     Each thread reads from a synchronous queue and runs the command in a
     shell.
     """
+    if len(input) == 0:
+        raise RuntimeError("No input files specified.")
 
-    with open(input) as fid:
-        for line in fid:
-            line = line.strip()
-            if len(line) == 0:
-                continue
-            q.put(line)
-            print(f"Command {line}")
+    q: "Queue[str]" = Queue()
+    file_lock = Lock()
+    fail_file = open(failed, "w")
+
+    def worker(t_id: int):
+        env = os.environ.copy()
+        env.update({"PYTHONUNBUFFERED": "1"})
+        while not q.empty():
+            cmd = q.get()
+            print(f"Executing command on thread {t_id}: {cmd}")
+            proc = subprocess.Popen(
+                cmd,
+                env=env,
+                shell=True,
+                text=True,
+                bufsize=1,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+            if proc.stdout is not None:
+                with proc.stdout:
+                    for line in iter(proc.stdout.readline, ""):
+                        sys.stdout.write(f"[T{t_id}]: {line}")
+                        sys.stdout.flush()
+            if proc.wait() != 0:
+                with file_lock:
+                    fail_file.write(cmd + "\n")
+            q.task_done()
+
+    for file in input:
+        with open(file) as fid:
+            for line in fid:
+                line = line.strip()
+                if len(line) == 0:
+                    continue
+                q.put(line)
+                print(f"Command {line}")
 
     threads = [Thread(target=worker, args=(i,)) for i in range(n_threads)]
     for t in threads:
