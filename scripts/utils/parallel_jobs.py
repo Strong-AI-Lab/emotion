557d0646cc55e747055ae30736367900ad3347e2
Aaron Keesing <akee511@aucklanduni.ac.nz>
2021-10-04 13:30:56 +1300

    Merge GPU and CPU parallel jobs scripts.

--- scripts/utils/parallel_jobs.py
+++ scripts/utils/parallel_jobs.py
@@ -1,48 +1,53 @@
 import os
 import subprocess
 import sys
+from datetime import datetime
 from pathlib import Path
 from queue import Queue
 from threading import Lock, Thread
 from typing import Tuple
 
 import click
+from click_option_group import optgroup, RequiredMutuallyExclusiveOptionGroup
 
 from ertk.utils import PathlibPath
 
 
 @click.command()
 @click.argument("input", type=PathlibPath(exists=True, dir_okay=False), nargs=-1)
-@click.option(
-    "--threads",
-    "n_threads",
-    type=int,
-    default=1,
-    help="Number of threads",
-    show_default=True,
-)
 @click.option("--failed", type=Path, help="Where to log failed commands.")
-def main(input: Tuple[Path], n_threads: int, failed: Path):
+@optgroup.group("Parallel method", cls=RequiredMutuallyExclusiveOptionGroup)
+@optgroup.option("--cpus", type=int, help="Number of CPU threads to use")
+@optgroup.option("--gpus", help="GPU IDs to run on.")
+def main(input: Tuple[Path], cpus: int, gpus: str, failed: Path):
     """Runs all commands specified in the INPUT file(s), splitting the
-    work across multiple threads such that each command runs solely on
-    whichever thread is next available.
+    work across multiple CPU threads or GPUs such that each command runs
+    solely on whichever thread/GPU is next available.
 
-    Each thread reads from a synchronous queue and runs the command in a
-    shell.
+    Each GPU still has it's own thread to run processed using
+    CUDA_VISIBLE_DEVICES. Each thread reads from a synchronous queue and
+    runs the command in a shell.
     """
     if len(input) == 0:
-        raise RuntimeError("No input files specified.")
+        raise ValueError("No input files specified.")
 
     q: "Queue[str]" = Queue()
     file_lock = Lock()
     fail_file = open(failed, "w")
 
-    def worker(t_id: int):
+    def worker(t_id: int = None, gpu: int = None):
         env = os.environ.copy()
         env.update({"PYTHONUNBUFFERED": "1"})
+        if gpu is not None:
+            env.update({"CUDA_VISIBLE_DEVICES": str(gpu)})
+        print_prefix = f"[T{t_id}]" if gpu is None else f"[GPU{gpu}]"
         while not q.empty():
             cmd = q.get()
-            print(f"Executing command on thread {t_id}: {cmd}")
+            print(
+                datetime.now().isoformat(),
+                f"Executing command on thread {t_id}: {cmd}",
+                flush=True,
+            )
             proc = subprocess.Popen(
                 cmd,
                 env=env,
@@ -55,7 +60,9 @@ def main(input: Tuple[Path], n_threads: int, failed: Path):
             if proc.stdout is not None:
                 with proc.stdout:
                     for line in iter(proc.stdout.readline, ""):
-                        sys.stdout.write(f"[T{t_id}]: {line}")
+                        sys.stdout.write(
+                            f"{datetime.now().isoformat()} {print_prefix}:{line}"
+                        )
                         sys.stdout.flush()
             if proc.wait() != 0:
                 with file_lock:
@@ -65,18 +72,21 @@ def main(input: Tuple[Path], n_threads: int, failed: Path):
 
     for file in input:
         with open(file) as fid:
-            for line in fid:
-                line = line.strip()
-                if len(line) == 0:
-                    continue
+            for line in list(filter(None, map(str.strip, fid))):
                 q.put(line)
-                print(f"Command {line}")
+    print(f"Loaded {q.qsize()} commands")
 
-    threads = [Thread(target=worker, args=(i,)) for i in range(n_threads)]
+    if cpus is not None:
+        threads = [Thread(target=worker, kwargs={"t_id": i}) for i in range(cpus)]
+    else:
+        _gpus = list(map(int, gpus.split(",")))
+        threads = [Thread(target=worker, kwargs={"gpu": i}) for i in _gpus]
+    print(f"Starting threads at {datetime.now().isoformat()}")
     for t in threads:
         t.start()
     for t in threads:
         t.join()
+    print(f"Finished at {datetime.now().isoformat()}")
 
 
 if __name__ == "__main__":
