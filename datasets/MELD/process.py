d3459609f7cd95af57a64bd1e5b2f77f2eb790fa
Aaron Keesing <akee511@aucklanduni.ac.nz>
2021-11-08 17:10:51 +1300

    Add MELD dataset.

---
+++ datasets/MELD/process.py
@@ -0,0 +1,65 @@
+"""Process the raw MELD dataset.
+
+This assumes the following file structure from the original compressed files:
+/.../
+    dev_splits_complete/
+        *.mp4
+    train_splits/
+        *.mp4
+    output_repeated_splits_test/
+        *.mp4
+    *.csv
+    ...
+"""
+
+from pathlib import Path
+
+import click
+import pandas as pd
+
+from ertk.dataset import write_annotations, write_filelist, resample_audio
+from ertk.utils import PathlibPath
+
+
+@click.command()
+@click.argument("input_dir", type=PathlibPath(exists=True, file_okay=False))
+@click.option("--resample/--noresample", default=True)
+def main(input_dir: Path, resample: bool):
+    """Process the MELD dataset at location INPUT_DIR and resample audio
+    to 16 kHz 16-bit WAV audio.
+    """
+    splits = ["train", "dev", "test"]
+    split_dirs = ["train_splits", "dev_splits_complete", "output_repeated_splits_test"]
+    csvs = ["train_sent_emo.csv", "dev_sent_emo.csv", "test_sent_emo.csv"]
+
+    name_map = {}
+    all_paths = []
+    for split, x in zip(splits, split_dirs):
+        paths = list(input_dir.glob(f"{x}/dia*.mp4"))
+        all_paths.extend(paths)
+        name_map.update({x: f"{split}_{x.stem}" for x in paths})
+
+    resample_dir = Path("resampled")
+    if resample:
+        resample_dir.mkdir(parents=True, exist_ok=True)
+        resample_audio(all_paths, resample_dir, mapping=name_map)
+
+    dfs = []
+    for split, x in zip(splits, csvs):
+        df = pd.read_csv(input_dir / x)
+        df.index = df.apply(
+            lambda x: f"{split}_dia{x['Dialogue_ID']}_utt{x['Utterance_ID']}", axis=1
+        )
+        dfs.append(df)
+    df = pd.concat(dfs)
+    df = df.drop(index={"dev_dia110_utt7"})
+
+    write_filelist({resample_dir / f"{x}.wav" for x in df.index}, "files_all")
+    write_annotations(df["Emotion"].to_dict(), "label")
+    write_annotations(df["Speaker"].to_dict(), "speaker")
+    write_annotations({x: "us" for x in df.index}, "country")
+    write_annotations({x: "en" for x in df.index}, "language")
+
+
+if __name__ == "__main__":
+    main()
