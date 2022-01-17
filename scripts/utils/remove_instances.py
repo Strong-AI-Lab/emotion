512d47180fee410dbe25b5b2d703fc952814d59b
Aaron Keesing <akee511@aucklanduni.ac.nz>
2022-01-17 14:49:19 +1300

    Correct typing for names.

--- scripts/utils/remove_instances.py
+++ scripts/utils/remove_instances.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import click
-import numpy as np
 
 from ertk.dataset import read_features, write_features
 from ertk.utils import PathlibPath
@@ -10,23 +9,23 @@ from ertk.utils import PathlibPath
 @click.command()
 @click.argument("input", type=PathlibPath(exists=True, dir_okay=False))
 @click.argument("output", type=Path, required=False)
-@click.option("--names", type=PathlibPath(exists=True, dir_okay=False))
-def main(input: Path, output: Path, names: Path):
+@click.option("--names", "names_file", type=PathlibPath(exists=True, dir_okay=False))
+def main(input: Path, output: Path, names_file: Path):
     """Remove instances from INPUT features that aren't in the NAMES
     file, and write to OUTPUT. If OUTPUT is not given, overwrites INPUT
     in-place.
     """
-    with open(names) as fid:
+    with open(names_file) as fid:
         keep_names = {Path(x.strip()).stem for x in fid}
     data = read_features(input)
-    idx = np.array([i for i, n in enumerate(data.names) if n in keep_names])
+    idx, names = zip(*[(i, n) for i, n in enumerate(data.names) if n in keep_names])
 
     if not output:
         output = input
     write_features(
         output,
         corpus=data.corpus,
-        names=data.names[idx],
+        names=names,
         features=data.features[idx],
         feature_names=data.feature_names,
     )
