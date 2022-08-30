60f1b88004ec27c9995d2406102b8ced71dbba50
Aaron Keesing <akee511@aucklanduni.ac.nz>
2022-08-31 01:48:56 +1200

    Update names tests.

--- tests/dataset/test_dataset.py
+++ tests/dataset/test_dataset.py
@@ -37,7 +37,7 @@ class TestDataset:
         assert data.n_speakers == 2
         assert data.speaker_names == ["1001", "1002"]
         assert len(data.names) == len(data)
-        assert data.names == all_clips_names
+        assert np.array_equal(data.names, all_clips_names)
         assert data.subset == "all"
         assert data.subsets.keys() == {"all", "subset1", "all_unsorted"}
         assert list(data.speaker_counts) == [6, 6]
@@ -47,7 +47,7 @@ class TestDataset:
 
     def test_unsorted(self):
         data = Dataset(test_data_dir / "corpus_info.yaml", subset="all_unsorted")
-        assert data.names == all_clips_unsorted_names
+        assert np.array_equal(data.names, all_clips_unsorted_names)
         assert list(data.speaker_counts) == [6, 6]
         assert data.subset == "all_unsorted"
 
@@ -67,7 +67,7 @@ class TestDataset:
         assert data.n_speakers == 2
         assert set(data.speaker_names) == {"1001", "1002"}
         assert len(data.names) == len(data)
-        assert data.names == subset_names
+        assert np.array_equal(data.names, subset_names)
         assert data.subset == "subset1"
         assert data.subsets.keys() == {"all", "subset1", "all_unsorted"}
 
@@ -89,14 +89,14 @@ class TestDataset:
         assert data.feature_names == feature_names
         assert data.n_features == len(data.feature_names)
         assert data.n_instances == len(data) == len(all_clips_names)
-        assert data.names == all_clips_names
+        assert np.array_equal(data.names, all_clips_names)
 
     def test_features_3d(self):
         data = Dataset(test_data_dir / "corpus_info.yaml", features="features_3d")
         assert np.allclose(data.x, features_3d)
         assert data.feature_names == feature_names
         assert data.n_instances == len(all_clips_names)
-        assert data.names == all_clips_names
+        assert np.array_equal(data.names, all_clips_names)
 
     def test_copy(self):
         data = Dataset(test_data_dir / "corpus_info.yaml", features="features_2d")
@@ -105,7 +105,7 @@ class TestDataset:
         assert data_copy.annotations.equals(data.annotations)
         assert data_copy.annotations is not data.annotations
         assert data_copy.subset == data.subset
-        assert data_copy.names == data.names
+        assert np.array_equal(data_copy.names, data.names)
         assert data_copy.names is not data.names
         assert data_copy.partitions == data.partitions
         assert data_copy.partitions is not data.partitions
@@ -212,7 +212,7 @@ class TestDataset:
         data.remove_instances(drop=["1001_DFA_HAP_XX", "1001_DFA_HAP_XX"])
         ref_names = all_clips_names.copy()
         ref_names.remove("1001_DFA_HAP_XX")
-        assert data.names == ref_names
+        assert np.array_equal(data.names, ref_names)
         assert list(data.speakers) == ["1001"] * 5 + ["1002"] * 6
         assert list(data.speaker_counts) == [5, 6]
         assert list(data.speaker_indices) == [0] * 5 + [1] * 6
@@ -225,7 +225,7 @@ class TestDataset:
     def test_keep_instances_no_features(self):
         data = Dataset(test_data_dir / "corpus_info.yaml")
         data.remove_instances(keep=["1001_DFA_HAP_XX", "1001_DFA_NEU_XX"])
-        assert data.names == ["1001_DFA_HAP_XX", "1001_DFA_NEU_XX"]
+        assert list(data.names) == ["1001_DFA_HAP_XX", "1001_DFA_NEU_XX"]
         assert len(data.get_annotations("speaker")) == 2
         assert list(data.speakers) == ["1001", "1001"]
         assert data.speaker_names == ["1001"]
@@ -301,7 +301,7 @@ class TestCombinedDataset:
         assert list(combined.corpus_indices) == [0] * 12 + [1] * 12
         ref_names = [f"test_corpus_{x}" for x in all_clips_names]
         ref_names += [f"test_corpus2_{x}" for x in all_clips_names]
-        assert combined.names == ref_names
+        assert np.array_equal(combined.names, ref_names)
         assert combined.speaker_names == [
             "test_corpus2_1001",
             "test_corpus2_1002",
