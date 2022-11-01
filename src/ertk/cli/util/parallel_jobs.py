540ee651f96e36714a296fd91fffc6e6685b8265
Aaron Keesing <akee511@aucklanduni.ac.nz>
2022-11-01 16:44:05 +1300

    Improvements to parallel_jobs

--- src/ertk/cli/util/parallel_jobs.py
+++ src/ertk/cli/util/parallel_jobs.py
@@ -15,7 +15,9 @@ from click_option_group import RequiredMutuallyExclusiveOptionGroup, optgroup
 @click.argument(
     "input", type=click.Path(exists=True, dir_okay=False, path_type=Path), nargs=-1
 )
-@click.option("--failed", type=Path, help="Where to log failed commands.")
+@click.option(
+    "--failed", type=Path, help="Where to log failed commands.", required=True
+)
 @optgroup.group("Parallel method", cls=RequiredMutuallyExclusiveOptionGroup)
 @optgroup.option("--cpus", type=int, help="Number of CPU threads to use")
 @optgroup.option("--gpus", help="GPU IDs to run on.")
@@ -33,13 +35,18 @@ def main(input: Tuple[Path], cpus: int, gpus: str, failed: Path):
 
     q: "Queue[str]" = Queue()
     file_lock = Lock()
+    if not failed.exists():
+        failed.parent.mkdir(exist_ok=True, parents=True)
     fail_file = open(failed, "w")
+    n_failed = 0
 
     def worker(t_id: int = None, gpu: int = None):
         env = os.environ.copy()
         env.update({"PYTHONUNBUFFERED": "1"})
         if gpu is not None:
             env.update({"CUDA_VISIBLE_DEVICES": str(gpu)})
+        else:  # CPU
+            env.update({"OMP_NUM_THREADS": "1"})
         print_prefix = f"[T{t_id}]" if gpu is None else f"[GPU{gpu}]"
         while not q.empty():
             cmd = q.get()
@@ -65,7 +72,9 @@ def main(input: Tuple[Path], cpus: int, gpus: str, failed: Path):
                         )
                         sys.stdout.flush()
             if proc.wait() != 0:
+                nonlocal n_failed
                 with file_lock:
+                    n_failed += 1
                     fail_file.write(cmd + "\n")
                     fail_file.flush()
             q.task_done()
@@ -74,7 +83,8 @@ def main(input: Tuple[Path], cpus: int, gpus: str, failed: Path):
         with open(file) as fid:
             for line in filter(None, map(str.strip, fid)):
                 q.put(line)
-    print(f"Loaded {q.qsize()} commands")
+    n_commands = q.qsize()
+    print(f"Loaded {n_commands} commands")
 
     if cpus is not None:
         threads = [Thread(target=worker, kwargs={"t_id": i}) for i in range(cpus)]
@@ -90,6 +100,7 @@ def main(input: Tuple[Path], cpus: int, gpus: str, failed: Path):
     total_time = datetime.now() - start_time
     print(f"Finished at {datetime.now().isoformat()}")
     print(f"Total time: {total_time} ({total_time.total_seconds()} s)")
+    print(f"{n_commands - n_failed} commands succeeded, {n_failed} failed")
 
 
 if __name__ == "__main__":
