7447df495282d0b696bdd1462e035c989112fd33
Aaron Keesing <akee511@aucklanduni.ac.nz>
2022-11-07 11:29:57 +1300

    Allow to write instance names to file.

--- src/ertk/cli/dataset/info.py
+++ src/ertk/cli/dataset/info.py
@@ -1,25 +1,46 @@
+import logging
 from pathlib import Path
 from typing import Tuple
 
 import click
 
-from ertk.cli._utils import dataset_args
+from ertk.cli._utils import dataset_args, debug_args
 from ertk.config import get_arg_mapping
-from ertk.dataset import load_multiple
+from ertk.dataset import DataLoadConfig, load_datasets_config, load_multiple
+
+
+def write_list(dataset, output_list):
+    with open(output_list, "w") as fid:
+        fid.write("\n".join(dataset.get_audio_paths()))
 
 
 @click.command()
 @dataset_args
+@debug_args
+@click.option("--output_list", type=Path)
 def main(
     corpus_info: Tuple[Path],
+    data_config: Path,
     subset: Tuple[str],
     sel_groups: Tuple[str],
     remove_groups: Tuple[str],
     map_groups: Tuple[str],
     clip_seq: int,
     pad_seq: int,
+    verbose: int,
+    output_list: Path,
 ):
     """Print info about a dataset or combination of datasets."""
+    if verbose > 0:
+        logging.basicConfig(level=logging.DEBUG if verbose > 1 else logging.INFO)
+
+    if data_config:
+        dataset = load_datasets_config(DataLoadConfig.from_file(data_config))
+        print(dataset)
+        if output_list:
+            write_list(dataset, output_list)
+        return
+
     if len(subset) == 1 and ":" not in subset[0]:
         dataset = load_multiple(corpus_info, subsets=subset[0], label="")
     else:
@@ -45,6 +66,8 @@ def main(
         dataset.pad_arrays(pad_seq)
 
     print(dataset)
+    if output_list:
+        write_list(dataset, output_list)
 
 
 if __name__ == "__main__":
